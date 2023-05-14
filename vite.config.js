316372b74d5f8f609abfe30314e0baa6f0a034f7
Lewis Campbell <info@lewiscampbell.tech>
2023-05-15 09:49:31 +1200

    building dist works again

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,36 @@ export default defineConfig({
 		solidPlugin(),
 		VitePWA({
 			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
+			},
 			devOptions: {
 				enabled: true
+			},
+			manifest: {
+				"name": "Lewis Campbell Technologies",
+				"icons": [
+					{
+						"src": "img/icon/icon192.png",
+						"type": "image/png",
+						"sizes": "192x192",
+						"purpose": "any maskable"
+					},
+					{
+						"src": "img/icon/icon144.png",
+						"type": "image/png",
+						"sizes": "144x144"
+					},
+					{
+						"src": "img/icon/icon512.png",
+						"type": "image/png",
+						"sizes": "512x512"
+					}
+				],
+				"start_url": "/",
+				"background_color": "hsl(200, 100%, 20%)",
+				"display": "minimal-ui",
+				"theme_color": "#000"
 			}
 		})
 	],
@@ -18,12 +46,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        sync: resolve(__dirname, 'sync.html'),
-        'new-old': resolve(__dirname, 'index-old.html')
+        main: 'www/index.html',
+        sync: 'www/sync.html',
+        old: 'www/index-old.html',
+        old2: 'www/index-old.html'
       }
     }
   }
 })
-
 
