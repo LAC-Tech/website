adc5af588317b985c9acfeabf459d810b7006085
Lewis Campbell <info@lewiscampbell.tech>
2023-08-25 15:28:06 +1200

    new devserve script works perfectly

--- devserve.sh
+++ devserve.sh
@@ -1,2 +1,2 @@
-#browser-sync start --port 8888 --no-notify false --proxy "localhost:2015" --files "public/**/*"
-ls input/* | entr -c make -B
+browser-sync www -w --no-notify
+#ls input/* | entr -c make -B
