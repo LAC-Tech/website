b68adcbd7803a8323d3b98dfd15bbc47c2738125
Lewis Campbell <info@lewiscampbell.tech>
2023-08-25 16:24:09 +1200

    cleaned up image makefile a bit

--- image.mk
+++ image.mk
@@ -1,16 +1,13 @@
 INPUT_DIR := static/img
 OUTPUT_DIR := www/img/hashed
 
-IMAGE_FILES := $(wildcard $(INPUT_DIR)/*.png) $(wildcard $(INPUT_DIR)/*.jpg)
-
-WEBP_FILES := $(IMAGE_FILES:$(INPUT_DIR)/%=$(OUTPUT_DIR)/%)
-WEBP_FILES := $(WEBP_FILES:%=%.webp)
+IMAGE_FILES := $(wildcard $(INPUT_DIR)/*.png $(INPUT_DIR)/*.jpg)
+WEBP_FILES := $(patsubst $(INPUT_DIR)/%,$(OUTPUT_DIR)/%.webp,$(IMAGE_FILES))
 
 all: $(WEBP_FILES)
 
 $(OUTPUT_DIR)/%.webp: $(INPUT_DIR)/% | $(OUTPUT_DIR)
 	convert $< $(OUTPUT_DIR)/$(notdir $(basename $*))-$(shell xxhsum $< | cut -d ' ' -f1).webp
-	cp $< $(OUTPUT_DIR)/$(notdir $(basename $*))-$(shell xxhsum $< | cut -d ' ' -f1)$(suffix $<)
 
 $(OUTPUT_DIR):
 	mkdir -p $(OUTPUT_DIR)
