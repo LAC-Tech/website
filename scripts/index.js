1d252909902b34d32f176c66815745a975f8a0ce
Lewis Campbell <info@lewiscampbell.tech>
2023-08-29 16:14:02 +1200

    fuck pandoc

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,42 @@
-import * as fs from 'node:fs/promises'
+#!/usr/bin/env node
+import fs from 'fs/promises';
+import { marked } from 'marked';
+import matter from 'gray-matter';
+import ejs from 'ejs';
 
-const templatePath = "../static/template.html.ejs"
+// Parse command line arguments
+const args = process.argv.slice(2);
+const templateIndex = args.indexOf('--template');
+const inputIndex = args.indexOf('--input');
+const outputIndex = args.indexOf('--output');
 
-const html = (title, body) => {
-	ejs.renderFile(templatePath, {title, body})
+if (templateIndex === -1 || inputIndex === -1 || outputIndex === -1) {
+  console.error('Usage: ./my_script.mjs --template a.html --input b.md --output c.html');
+  process.exit(1);
 }
 
-if (4 > process.argv.length) {
-	console.error("expecting 3 args: type, input path, output path")
-	process.exit(1)
-}
+const templateFilePath = args[templateIndex + 1];
+const inputFilePath = args[inputIndex + 1];
+const outputFilePath = args[outputIndex + 1];
 
-const [_, type, outputPath, contentsPath, metaDataPath] = process.argv
+// Read template file
+const templateContent = await fs.readFile(templateFilePath, 'utf-8');
 
-if (type == 'html') {
-	if (!metaDataPath) {
-		console.error("html requires associated json metadata file")
-		process.exit(1)
-	}
-	const metaData = fs.readFile(metaDataPath)
-	const contents = html(
+// Read input Markdown file and extract front matter
+const inputContent = await fs.readFile(inputFilePath, 'utf-8');
+const { data, content } = matter(inputContent);
 
-	fs.writeFile(
-} else if (type == 'md') {
-	fs
-} else {
-	console.error("type must be either html or md")
-	process.exit(1)
-}
+// Convert Markdown content to HTML
+const body = marked(content);
+
+// Render the template using EJS
+const renderedTemplate = ejs.render(templateContent, {
+  title: data.title || '',
+  body,
+});
+
+// Write the final HTML to the output file
+await fs.writeFile(outputFilePath, renderedTemplate, 'utf-8');
+
+console.log(`File '${outputFilePath}' generated successfully.`);
+
