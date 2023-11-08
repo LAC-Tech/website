3ebc7135ff9f6f69e199d99b64b4925684d2d8f9
Lewis Campbell <info@lewiscampbell.tech>
2023-11-09 09:48:52 +1300

    google still seems to recommend png icons, so modified image makefile to generate them too

--- image.mk
+++ image.mk
@@ -1,17 +1,20 @@
 INPUT_DIR := static/img
 OUTPUT_DIR := www/img/hashed
 
-IMAGE_FILES := $(wildcard $(INPUT_DIR)/*.png $(INPUT_DIR)/*.jpg)
-WEBP_FILES := $(patsubst $(INPUT_DIR)/%,$(OUTPUT_DIR)/%.webp,$(IMAGE_FILES))
+INPUT_FILES := $(wildcard $(INPUT_DIR)/*.png $(INPUT_DIR)/*.jpg)
+WEBP_FILES := $(patsubst $(INPUT_DIR)/%,$(OUTPUT_DIR)/%.webp,$(INPUT_FILES))
+PNG_FILES := $(patsubst $(INPUT_DIR)/%.png,$(OUTPUT_DIR)/%.png,$(INPUT_FILES))
 
-all: $(WEBP_FILES)
+all: $(WEBP_FILES) $(PNG_FILES)
 
 $(OUTPUT_DIR)/%.webp: $(INPUT_DIR)/% | $(OUTPUT_DIR)
 	convert $< $(OUTPUT_DIR)/$(notdir $(basename $*))-$(shell xxhsum $< | cut -d ' ' -f1).webp
+
+$(OUTPUT_DIR)/%.png: $(INPUT_DIR)/%.png | $(OUTPUT_DIR)
+	cp $< $(OUTPUT_DIR)/$(notdir $(basename $*))-$(shell xxhsum $< | cut -d ' ' -f1).png
 
 $(OUTPUT_DIR):
 	mkdir -p $(OUTPUT_DIR)
 
 clean:
 	rm -rf $(OUTPUT_DIR)
-
